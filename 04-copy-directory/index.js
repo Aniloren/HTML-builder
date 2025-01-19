a48f3400f508060378a58e88180245dd8e265230
Aniloren <enderxoma2014@yandex.ru>
2025-01-19 14:14:54 +0300

    feat: add 04-copy-directory solution

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPathForCopying = path.join(__dirname, 'files');
+const folderPathCopied = path.join(__dirname, 'files-copy');
+
+//removing folder
+fs.rm(folderPathCopied, { recursive: true, force: true }, (err) => {
+  if (err) {
+    console.error('Error removing directory:', err);
+    return;
+  };
+
+  //creating folder
+  fs.mkdir(folderPathCopied, { recursive: true }, (err) => {
+    if (err) {
+      console.error('Error creating directory:', err);
+      return
+    }
+
+    //copying files
+    fs.readdir(folderPathForCopying, (err, files) => {
+      if (err) throw err;
+      files.forEach((file) => {
+
+        fs.copyFile(
+          path.join(folderPathForCopying, file),
+          path.join(folderPathCopied, file),
+          (err) => {
+            if (err) throw err;
+            console.log(file);
+          },
+        );
+      });
+    });
+  });
+
+
+});
+
+
+
