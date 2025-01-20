678d517d24a772d68d5bd06750cd16ed176b41c3
Aniloren <enderxoma2014@yandex.ru>
2025-01-20 23:10:09 +0300

    refactor: refactor code 1 task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,16 +1,7 @@
 const fs = require("fs");
 const path = require('path');
 
-//first way
-// const readableStream = fs.createReadStream(`${process.argv[1]}/text.txt`, 'utf-8');
-// readableStream.on("data", (data) => console.log(data));
-
-//second way
-// const readableStream = fs.createReadStream(`${__dirname}/text.txt`, 'utf-8');
-// readableStream.on("data", (data) => console.log(data));
-
-//third way
-const readableStream = fs.createReadStream(`${path.dirname(__filename)}/text.txt`, 'utf-8');
+const readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 readableStream.on("data", (data) => console.log(data));
 
 readableStream.on('error', (error) => {
