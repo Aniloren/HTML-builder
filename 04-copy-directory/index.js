22e2373a9ec2d1ac7f33da74a27ea8a01dbd4761
Aniloren <enderxoma2014@yandex.ru>
2025-01-21 02:21:09 +0300

    fix: fix bug 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,34 +5,36 @@ const folderPathForCopying = path.join(__dirname, 'files');
 const folderPathCopied = path.join(__dirname, 'files-copy');
 
 function copyDirectory(sourcePath, resultPath) {
-  fs.mkdir(resultPath, { recursive: true }, (err) => {
-    if (err) {
-      console.error('Error creating folder', err);
-    }
-    fs.readdir(sourcePath, { withFileTypes: true }, (err, files) => {
+  fs.rm(folderPathCopied, { recursive: true, force: true }, () => {
+    fs.mkdir(resultPath, { recursive: true }, (err) => {
       if (err) {
-        console.error('Error reading the directory', err);
-        return;
-      } else {
-        files.forEach((file) => {
-          const sourceFile = path.join(sourcePath, file.name);
-          const resultFile = path.join(resultPath, file.name);
-          fs.stat(sourceFile, (err, stat) => {
-            if (err) {
-              console.error('Error reading stat', err);
-            }
-            if (stat.isDirectory()) {
-              copyDirectory(sourceFile, resultFile);
-            } else {
-              fs.copyFile(sourceFile, resultFile, (err) => {
-                if (err) {
-                  console.error('Error copying a file', err);
-                }
-              });
-            }
-          });
-        });
+        console.error('Error creating folder', err);
       }
+      fs.readdir(sourcePath, { withFileTypes: true }, (err, files) => {
+        if (err) {
+          console.error('Error reading the directory', err);
+          return;
+        } else {
+          files.forEach((file) => {
+            const sourceFile = path.join(sourcePath, file.name);
+            const resultFile = path.join(resultPath, file.name);
+            fs.stat(sourceFile, (err, stat) => {
+              if (err) {
+                console.error('Error reading stat', err);
+              }
+              if (stat.isDirectory()) {
+                copyDirectory(sourceFile, resultFile);
+              } else {
+                fs.copyFile(sourceFile, resultFile, (err) => {
+                  if (err) {
+                    console.error('Error copying a file', err);
+                  }
+                });
+              }
+            });
+          });
+        }
+      });
     });
   });
 }
